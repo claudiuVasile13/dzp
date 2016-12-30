5e7e0c4898de46debf4b21ba836f35a7f454f553
claudiu <claudiu.vasile13@yahoo.com>
2016-12-30 14:42:49 +0200

    edit profile

--- app/Http/Controllers/Backend/UsersController.php
+++ app/Http/Controllers/Backend/UsersController.php
@@ -10,9 +10,11 @@ use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Validator;
 use Intervention\Image\ImageManagerStatic as Image;
+use Illuminate\Support\Facades\Hash;
 
 class UsersController extends Controller
 {
@@ -57,22 +59,23 @@ class UsersController extends Controller
     {
         $user = User::find($user_id);
         if(count($user)) {
+            // Trim the input data
+//            return $request->all();
+            $request->merge(array_map('trim', Input::except('remove-friends', '_token')));
             $rules = [
                 'email' => 'required|email',
                 'username' => 'required|min:4|max:20',
                 'country' => 'required',
                 'gender' => 'required',
                 'image' => 'mimes:jpg,jpeg,png',
-                'signature' => 'mimes:jpg,jpeg,png'
+                'signature' => 'mimes:jpg,jpeg,png',
+                'password' => 'min:6|max:40|confirmed',
             ];
             $validator = Validator::make($request->all(), $rules);
             if ($validator->fails()) {
                 return Redirect::back()
                     ->withErrors($validator);
             } else {
-                // Trim the input data
-                $request->merge(array_map('trim', $request->all()));
-
                 $userImageName = null;
                 $userSignatureName = null;
 
@@ -92,11 +95,6 @@ class UsersController extends Controller
                     $userSignatureName = $imageName;
                 }
 
-                return $request->input('password');
-                if($request->input('password')) {
-
-                }
-
                 $profileData = [
                     'countryID' => $request->input('country'),
                     'gender' => $request->input('gender'),
@@ -104,7 +102,6 @@ class UsersController extends Controller
                     'username' => $request->input('username'),
                     'job_hobbies' => $request->input('job_hobbies'),
                     'gameranger_id' => $request->input('gameranger_id'),
-                    'birthday' => $request->input('birthday'),
                     'status' => $request->input('status'),
                     'description' => $request->input('description'),
                     'email' => $request->input('email'),
@@ -112,6 +109,11 @@ class UsersController extends Controller
                     'facebook' => $request->input('facebook'),
                     'twitter' => $request->input('twitter')
                 ];
+                if($request->input('brithday')) {
+                    $profileData['birthday'] = $request->input('birthday');
+                } else {
+                    $profileData['birthday'] = null;
+                }
                 if($userImageName !== null) {
                     $profileData['user_image'] = $userImageName;
                 }
@@ -119,8 +121,26 @@ class UsersController extends Controller
                 ($userSignatureName !== null) {
                     $profileData['signature'] = $userSignatureName;
                 }
+                if($request->has('password')) {
+                    $profileData['password'] = Hash::make($request->input('password'));;
+                }
 
                 $user->update($profileData);
+
+                if(count($request->input('remove-friends'))) {
+                    $friends = Friendship::friends($user_id);
+                    $removeFriends = [];
+                    if(count($friends)) {
+                       foreach ($request->input('remove-friends') as $removeFriend) {
+                           foreach($friends as $friend) {
+                               if($friend->user_id == $removeFriend) {
+                                   $removeFriends[] = $removeFriend;
+                               }
+                            }
+                       }
+                       Friendship::destroy($removeFriends);
+                    }
+                }
                 return Redirect::to('/admin-panel/users')
                     ->with('EditProfileSuccess', 'You successfully edited the user ' . $user->username);
             }
